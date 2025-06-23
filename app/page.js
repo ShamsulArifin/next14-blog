6780246e1dd5f331e11fab49706b410e4c318503
Shamsul Arifin <arifinbd6@gmail.com>
2025-06-23 11:25:25 +0600

    12. Rendering List of Elements

--- app/page.js
+++ app/page.js
@@ -11,35 +11,37 @@ function Card({ children }) {
 
 export default function Home() {
   const [isVisible, setIsVisible] = useState(true);
-
+  const [names, setNames] = useState(['Pitor', 'John', 'Terry']);
   const name = 'Piotr';
   const handleClick = () => {
     setIsVisible(!isVisible);
   };
+  const handleAdd = () => {
+    setNames([...names, 'New Element']);
+  };
 
-  const cards = isVisible && (
-    <>
-      <Card>This is being passed from parent</Card>
-      <Card>
-        <div>This is JS!</div>
-        <Card>Nested text!</Card>
-      </Card>
-      <Card />
-      <Card />
-    </>
-  );
+  const cards =
+    isVisible && names.map((name, index) => <Card key={index}>{name}</Card>);
 
   return (
     <>
       <div className='p-20 space-y-4'>
         <div>Hello, {name}</div>
         {cards}
-        <button
-          className='border rounded-md border-gray-600 p-3'
-          onClick={handleClick}
-        >
-          {isVisible ? 'Hide' : 'Show'}
-        </button>
+        <div className='flex space-x-4'>
+          <button
+            className='border rounded-md border-gray-600 p-3'
+            onClick={handleClick}
+          >
+            {isVisible ? 'Hide' : 'Show'}
+          </button>
+          <button
+            className='border rounded-md border-gray-600 p-3'
+            onClick={handleAdd}
+          >
+            Add
+          </button>
+        </div>
       </div>
     </>
   );
