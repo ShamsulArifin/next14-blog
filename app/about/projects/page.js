6f5ce6314cb91957fbed5af8d35030857e9c54b1
Shamsul Arifin <arifinbd6@gmail.com>
2025-07-08 16:44:06 +0600

    25. Fetch Cache in Practice

--- app/about/projects/page.js
+++ app/about/projects/page.js
@@ -1,14 +1,16 @@
 export default async function ProjectsPage() {
-  const response = await fetch('http://localhost:3001/repos')
-  const repos = await response.json()
+  const response = await fetch('http://localhost:3001/repos', {
+    next: { revalidate: 3600 },
+  });
+  const repos = await response.json();
 
   return (
-    <div className="p-20">
-      <h1 className="mb-8 text-xl">Projects</h1>
+    <div className='p-20'>
+      <h1 className='mb-8 text-xl'>Projects</h1>
 
       <ul>
-        {repos.map(repo => (
-          <li key={repo.id} className="mb-4">
+        {repos.map((repo) => (
+          <li key={repo.id} className='mb-4'>
             <div>{repo.title}</div>
             <div>{repo.description}</div>
             <div>{repo.stargazers_count}</div>
